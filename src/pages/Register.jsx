d213bac4cc74cb2bbc1c09d6ae52d79640cd749f
sumit sinha <sumit31aug2002@gmail.com>
2026-02-25 11:14:11 +0530

    fixed register phone issue

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,28 +4,43 @@ import axios from "axios";
 
 function Register() {
   const navigate = useNavigate();
+
   const [form, setForm] = useState({
     username: "",
     email: "",
     password: "",
+    phone: "",
   });
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    console.log("Submitting:", form); // Check this in console
+
     try {
       await axios.post(
         `${import.meta.env.VITE_API_BASE_URL}/api/users/register/`,
-        form
+        form,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
       );
 
-      alert("Registration successful. Please login.");
+      alert("Registration successful");
       navigate("/login");
     } catch (err) {
+      console.log("Backend Error:", err.response?.data);
       alert("Registration failed");
     }
   };
@@ -33,23 +48,44 @@ function Register() {
   return (
     <div>
       <h2>Register</h2>
+
       <form onSubmit={handleSubmit}>
         <input
+          type="text"
           name="username"
           placeholder="Username"
+          value={form.username}
           onChange={handleChange}
+          required
         />
+
         <input
+          type="text"
+          name="phone"
+          placeholder="phone"
+          value={form.phone}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="email"
           name="email"
           placeholder="Email"
+          value={form.email}
           onChange={handleChange}
+          required
         />
+
         <input
-          name="password"
           type="password"
+          name="password"
           placeholder="Password"
+          value={form.password}
           onChange={handleChange}
+          required
         />
+
         <button type="submit">Register</button>
       </form>
 
